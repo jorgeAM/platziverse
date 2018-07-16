1574aee083a5bbdf36badc831c28eb1a16f18312
Jorge L <jorge.alfmur@gmail.com>
2018-07-15 21:46:57 -0500

    modulo para conexión de nodejs y postgresql

---
+++ platziverse-db/lib/db.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+let sequelize = null;
+
+module.exports = function setupDataBase(config) {
+  if (!sequelize) sequelize = new Sequelize(config);
+  return sequelize;
+};
