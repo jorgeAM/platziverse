e3dfe2c91d0f924414850d9527bab52678ea5b5d
Jorge L <jorge.alfmur@gmail.com>
2018-07-15 22:50:40 -0500

    relacionamos Agent con Metric

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,4 +1,19 @@
-module.exports = function (config) {
+const setupDataBase = require('./lib/db');
+const setupAgentModel = require('./models/agent');
+const setupMetrictModel = require('./models/metric');
+
+module.exports = async function (config) {
+  const sequelize = setupDataBase(config);
+  const AgentModel = setupAgentModel(config);
+  const MetricModel = setupMetrictModel(config);
+  /*RELACIÓNES (AGREGA AL MODELO LA FORIGN KEY)*/
+  AgentModel.hasMany(MetricModel);
+  MetricModel.belongsTo(AgentModel);
+
+  await sequelize.authenticate();
+  /*método que crea los modelos en la DB*/
+  sequelize.sync();
+
   const Agent = {};
   const Metric = {};
 
