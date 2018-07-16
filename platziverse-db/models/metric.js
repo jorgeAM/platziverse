c7848a74d8bd64d783782dfb940d26e4ba3c6a53
Jorge L <jorge.alfmur@gmail.com>
2018-07-15 22:18:17 -0500

    creación de modelos

---
+++ platziverse-db/models/metric.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const setupDataBase = require('../lib/db');
+
+module.exports = function setupMetricModel(config) {
+  const sequelize = setupDataBase(config);
+  return sequelize.define('metric', {
+    type: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    value: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+  });
+};
